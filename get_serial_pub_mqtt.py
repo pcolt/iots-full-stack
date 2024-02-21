2f05174ed58b39add115f7c2cb59496ddb5b8866
Pablo Colturi Esteve <pcolturi@proton.me>
2024-02-21 18:28:49 +0100

    Create get_serial_pub_mqtt.py

---
+++ get_serial_pub_mqtt.py
@@ -0,0 +1,11 @@
+import serial
+import time
+import paho.mqtt.publish as publish
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # connect to Arduino via serial communication
+ser.reset_input_buffer()                              # empty the buffer before start
+
+while True:
+  line = ser.readline().decode('utf-8').rstrip()      
+  print(line)                                                   # print value to terminal
+  publish.single("mio_topic", line, hostname="192.168.1.65")    # pubblish value to MQTT broker

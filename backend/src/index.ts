dc56ef1c973700e9b2a2273993739c384d4a07d7
pcolt <pcolturi@proton.me>
2024-03-01 18:50:38 +0100

    remove comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,14 +5,10 @@ import measurementsRouter from './controllers/measures_controller'
 
 const port = process.env.PORT
 
-// await main()
-
 const app = express()
 app.use(express.static('../frontend/dist'))     // when http GET request to main root or index.html it returns the static files in /dist build with vite
-app.use(cors())                     // allow cors
+app.use(cors())                                 // allow cors
 // app.use(express.json())
-
-// erry
 
 app.use('/measures', measurementsRouter)
 
